fbbdf765658b4798d20257bb937b510f3d78d3b4
abdul malek <143941431+malek-cdda@users.noreply.github.com>
2024-01-22 17:33:40 +0600

    update location

--- src/components/distance/Distance.tsx
+++ src/components/distance/Distance.tsx
@@ -52,21 +52,38 @@ const Distance = () => {
       }
       let startLocation; // Example coordinates for San Francisco
       let endLocation; // Example coordinates for Oakland
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-          console.log(position.coords.latitude, position.coords.longitude);
-          startLocation = new google.maps.LatLng(
-            position.coords.latitude,
-            position.coords.longitude
-          );
-          endLocation = new google.maps.LatLng(
-            position.coords.latitude + 1,
-            position.coords.longitude + 1
-          );
-          console.log(startLocation);
-          calculateAndDisplayRoute(startLocation, endLocation);
-        });
-      }
+      const input = document.getElementById("pac-input") as HTMLInputElement;
+
+      const options = {
+        fields: ["address_components", "geometry", "icon", "name", "place_id"],
+        strictBounds: false,
+      };
+      // find place library  = autcomplete
+      const autocomplete = new google.maps.places.Autocomplete(input, options);
+      autocomplete.bindTo("bounds", map);
+      autocomplete.addListener("place_changed", () => {
+        const place: any = autocomplete.getPlace();
+
+        if (!place?.geometry?.location) {
+          window.alert("wrong address");
+          return place;
+        }
+        if (navigator?.geolocation) {
+          navigator.geolocation.getCurrentPosition(function (position) {
+            console.log(position.coords.latitude, position.coords.longitude);
+            startLocation = new google.maps.LatLng(
+              position.coords.latitude,
+              position.coords.longitude
+            );
+            endLocation = new google.maps.LatLng(
+              place.geometry.location.lat(),
+              place.geometry.location.lng()
+            );
+            console.log(startLocation);
+            calculateAndDisplayRoute(startLocation, endLocation);
+          });
+        }
+      });
     }
     window.initMap = initMap;
     if (typeof google !== "undefined") {
@@ -83,6 +100,11 @@ const Distance = () => {
     });
   }, [latLng, placeId, state]);
   console.log(state);
-  return <div id="map" className="h-96"></div>;
+  return (
+    <>
+      <div id="map" className="h-96"></div>
+      <input id="pac-input" />
+    </>
+  );
 };
 export default Distance;
