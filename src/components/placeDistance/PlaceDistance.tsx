d2e3ebe62e14771090cd8c7208a8ff804369e70e
abdul malek <143941431+malek-cdda@users.noreply.github.com>
2024-01-23 17:12:56 +0600

    update distance route

--- src/components/placeDistance/PlaceDistance.tsx
+++ src/components/placeDistance/PlaceDistance.tsx
@@ -1,61 +1,79 @@
 "use client";
-import { use, useEffect } from "react";
+import { use, useEffect, useState } from "react";
 import { declareMap } from "../map/map";
 
 const PlaceDistance = () => {
+  const [distance, setDistance] = useState("");
   useEffect(() => {
     async function initMap() {
       const { map } = await declareMap("map");
-      const startLocation = {
-        lat: 45.4222931,
-        lng: -75.6870167,
-      };
-
-      const endLocation = {
-        lat: 45.4199184,
-        lng: -75.710681,
-      };
       const directionsService = new google.maps.DirectionsService();
       const directionsRenderer = new google.maps.DirectionsRenderer();
-
-      directionsService.route(
-        {
-          origin: startLocation,
-          destination: endLocation,
-          travelMode: google.maps.TravelMode.DRIVING,
-        },
-        function (response: any, status: any) {
-          if (status === google.maps.DirectionsStatus.OK) {
-            const steps = response.routes[0].overview_path;
-
-            console.log(steps);
-            const marker = new google.maps.Marker({
-              map: map,
-              position: {
-                lat: steps[0].lat(),
-                lng: steps[0].lng(),
-              },
-              label: "🚘",
-              zIndex: 1,
-            });
-            let i = 0;
-            const interval = setInterval(function () {
-              i++;
-              if (i === steps.length) {
-                clearInterval(interval);
-                return;
-              }
-
-              marker.setPosition({
-                lat: steps[i].lat(),
-                lng: steps[i].lng(),
-              });
-            }, 1000);
-            directionsRenderer.setDirections(response);
-            directionsRenderer.setMap(map);
-          }
+      const input = document.getElementById("pac-input") as HTMLInputElement;
+      const options = {
+        fields: ["address_components", "geometry", "icon", "name", "place_id"],
+        strictBounds: false,
+      };
+      // find place library  = autcomplete
+      const autocomplete = new google.maps.places.Autocomplete(input, options);
+      autocomplete.bindTo("bounds", map);
+      autocomplete.addListener("place_changed", () => {
+        const place: any = autocomplete.getPlace();
+        if (!place?.geometry?.location) {
+          window.alert("wrong address");
+          return place;
         }
-      );
+
+        if (navigator?.geolocation) {
+          navigator.geolocation.getCurrentPosition(function (position) {
+            directionsService.route(
+              {
+                origin: {
+                  lat: position.coords.latitude,
+                  lng: position.coords.longitude,
+                },
+                destination: {
+                  lat: place.geometry.location.lat(),
+                  lng: place.geometry.location.lng(),
+                },
+                travelMode: google.maps.TravelMode.DRIVING,
+              },
+              function (response: any, status: any) {
+                if (status === google.maps.DirectionsStatus.OK) {
+                  const steps = response.routes[0].overview_path;
+                  console.log(steps);
+                  setDistance(response.routes[0]?.legs[0]?.distance?.text);
+                  const marker = new google.maps.Marker({
+                    map: map,
+                    position: {
+                      lat: steps[0].lat(),
+                      lng: steps[0].lng(),
+                    },
+                    label: "🛒",
+                    zIndex: 1,
+                  });
+                  let i = 0;
+                  const interval = setInterval(function () {
+                    i++;
+                    if (i === steps.length) {
+                      clearInterval(interval);
+                      return;
+                    }
+
+                    // setDistance(step.distance.text);
+                    marker.setPosition({
+                      lat: steps[i]?.lat(),
+                      lng: steps[i]?.lng(),
+                    });
+                  }, 100);
+                  directionsRenderer.setDirections(response);
+                  directionsRenderer.setMap(map);
+                }
+              }
+            );
+          });
+        }
+      });
     }
     window.initMap = initMap;
     if (typeof google !== "undefined") {
@@ -63,8 +81,17 @@ const PlaceDistance = () => {
     }
   }, []);
   return (
-    <div className="container mx-auto my-5">
-      <div id="map" className="h-[400px]"></div>
+    <div className="container mx-auto relative">
+      <div id="map" className="h-96"></div>
+      <div className="flex justify-center my-5  flex-col items-center absolute -top-4 left-1/2 transform -translate-x-1/2 w-1/2">
+        {/* <label className="capitalize  w-full px-2 ">destination </label> */}
+        <input
+          id="pac-input"
+          className="border rounded-xl px-5 py-2 w-full focus:outline-none focus:rounded-t-xl focus:rounded-none"
+          placeholder="enter your Destination city"
+        />
+      </div>
+      <h1 className="text-center"> City Distance {distance} </h1>
     </div>
   );
 };
